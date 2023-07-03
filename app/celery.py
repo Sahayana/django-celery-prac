ccd823b4aabd26119d2394cd75b2c26d0ad12a85
sahayana <saokhueyang@gmail.com>
2023-07-04 00:33:15 +0900

    변경: Celery app project changed

--- app/celery.py
+++ app/celery.py
@@ -8,7 +8,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('proj')
+app = Celery('app')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
